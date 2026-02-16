6ce9ab1c1b20a7be742deddb7680c50a2b8b6b4e
Godwin Malinde <malindegodwin9@gmail.com>
2026-02-16 18:46:28 +0300

    initial commit

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,8 +4,9 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
-print(get_response.text) # print raw text response
+get_response = requests.get(endpoint, params={'abc': 123}, 
+    json={'query': 'Hello World'})
+# print(get_response.text) # print raw text response
 
-print(get_response.json()['message'])
-print(get_response.status_code)
+print(get_response.json())
+# print(get_response.status_code)
