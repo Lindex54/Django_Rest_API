beb8d9db66ff73518d4505f214704375c42b3800
Godwin Malinde <malindegodwin9@gmail.com>
2026-02-24 18:36:36 +0300

    initial commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,21 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 # Create your views here
 
 def api_home(request, *args, **kwargs):
-    print(request.GET)
-    print(request.POST)
-    body = request.body
-    data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    model_data = Product.object.all().order-by("?").first()
+    # print(request.GET)
+    # print(request.POST)
+    # body = request.body
+    # data = {}
+    # try:
+    #     data = json.loads(body)
+    # except:
+    #     pass
+    # print(data)
+    # data['params'] = dict(request.GET)
+    # data['headers'] = dict(request.headers)
+    # data['content_type'] = request.content_type
     # return JsonResponse({'message': 'Hi there , this is your Django API response!!'})
     return JsonResponse(data)
