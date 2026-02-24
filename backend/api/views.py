a8c085520af92b3c031633a0b9e4cbf5348e0e1e
Godwin Malinde <malindegodwin9@gmail.com>
2026-02-24 19:12:06 +0300

    initial commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,21 @@
 import json
-from django.http import JsonResponse
+#from django.http import JsonResponse
+from rest_framework.response import Response
 from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
 # Create your views here
 
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.object.all().order-by("?").first()
-    # print(request.GET)
-    # print(request.POST)
-    # body = request.body
-    # data = {}
-    # try:
-    #     data = json.loads(body)
-    # except:
-    #     pass
-    # print(data)
-    # data['params'] = dict(request.GET)
-    # data['headers'] = dict(request.headers)
-    # data['content_type'] = request.content_type
-    # return JsonResponse({'message': 'Hi there , this is your Django API response!!'})
+
+    
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
     return JsonResponse(data)
